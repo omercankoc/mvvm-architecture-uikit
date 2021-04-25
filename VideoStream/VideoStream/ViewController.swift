a4f262e1ce8098063a6cb409f2af173ea9adc34b
Ömer Can Koç <63169919+omercankoc@users.noreply.github.com>
2021-04-25 23:55:15 +0300

    Created Project

---
+++ VideoStream/VideoStream/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VideoStream
+//
+//  Created by Gökberk Karakavuz on 25.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
