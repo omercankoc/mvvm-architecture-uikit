b38a1c181cbb802a8085c0ba86326773a9fa8ddb
Ömer Can Koç <63169919+omercankoc@users.noreply.github.com>
2021-09-14 21:37:09 +0300

    Created!

---
+++ MVVMArchitecture/MVVMArchitecture/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVMArchitecture
+//
+//  Created by Ömer Can Koç on 14.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
